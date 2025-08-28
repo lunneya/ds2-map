8f0660264470905e81b649cf420a70e9a6eb8b70
lunneya <lunneya.nivarre@gmail.com>
2025-08-29 00:26:02 +0500

    add a little styles, list with locations from original games, without DLC

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,17 @@
 import styles from './Sidebar.module.css';
+import { locations } from '../../data/locations.js';
 
-function Sidebar({ children, position = 'left' }) {
+function Sidebar() {
     return (
-        <aside className={`${styles.sidebar} ${styles[position]}`}>
-            {children}
+        <aside className={styles.sidebar}>
+            <h2 className={styles.title}>Locations</h2>
+            <ul>
+                {locations.map(loc => (
+                        <li key={loc.id} className={styles.locationsList}>
+                            {loc.name}
+                        </li>
+                    ))}
+            </ul>
         </aside>
     );
 }
--- src/data/locations.js
+++ src/data/locations.js
@@ -1,18 +1,154 @@
 export const locations = [
     {
+        id: 0,
+        name: 'Things Betwixt',
+    },
+    {
         id: 1,
-        name: 'Маджула',
-        x: 500,
-        y: 300,
-        description: 'Главный хаб игры',
-        bosses: [1],
+        name: 'Majula',
+        // x: 500,
+        // y: 300,
     },
     {
         id: 2,
         name: 'Forest of Fallen Giants',
-        x: 700,
-        y: 250,
-        description: 'Начальная боевая зона с первыми врагами',
-        bosses: [2, 3],
+        // x: 700,
+        // y: 250,
     },
-]
+    {
+        id: 3,
+        name: 'Heide\'s Tower of Flame'
+    },
+    {
+        id: 4,
+        name: 'Cathedral of Blue',
+    },
+    {
+        id: 5,
+        name: 'No-man\'s Wharf',
+    },
+    {
+        id: 6,
+        name: 'The Lost Bastille',
+    },
+    {
+        id: 7,
+        name: 'Belfry Luna',
+    },
+    {
+        id: 8,
+        name: 'Sinners\' Rise',
+    },
+    {
+        id: 9,
+        name: 'Huntsman\'s Copse',
+    },
+    {
+        id: 10,
+        name: 'Undead Purgatory',
+    },
+    {
+        id: 11,
+        name: 'Harvest Valley',
+    },
+    {
+        id: 12,
+        name: 'Earthen Peak',
+    },
+    {
+        id: 13,
+        name: 'Iron Keep',
+    },
+    {
+        id: 14,
+        name: 'Belfry Sol',
+    },
+    {
+        id: 15,
+        name: 'The Pit',
+    },
+    {
+        id: 16,
+        name: 'Grave of Saints',
+    },
+    {
+        id: 17,
+        name: 'The Gutter',
+    },
+    {
+        id: 18,
+        name: 'Black Gulch',
+    },
+    {
+        id: 19,
+        name: 'Shaded Woods',
+    },
+    {
+        id: 20,
+        name: 'Doors of Pharros',
+    },
+    {
+        id: 21,
+        name: 'Brightstone Cove Tseldora',
+    },
+    {
+        id: 22,
+        name: 'Brightstone Cove Tseldora',
+    },
+    {
+        id: 23,
+        name: 'Lord\'s Private Chamber',
+    },
+    {
+        id: 24,
+        name: 'Shrine of Winter',
+    },
+    {
+        id: 25,
+        name: 'Drangleic Castle',
+    },
+    {
+        id: 26,
+        name: 'Shrine of Amana',
+    },
+    {
+        id: 27,
+        name: 'Undead Crypt',
+    },
+    {
+        id: 28,
+        name: 'Aldia\'s Keep',
+    },
+    {
+        id: 29,
+        name: 'Dragon Aerie',
+    },
+    {
+        id: 30,
+        name: 'Dragon Shrine',
+    },
+    {
+        id: 31,
+        name: 'Dark Chasm of Old',
+    },
+    {
+        id: 32,
+        name: 'Memory of Vammar',
+    },
+    {
+        id: 33,
+        name: 'Memory of Orro',
+    },
+    {
+        id: 34,
+        name: 'Memory of Jeigh',
+    },
+    {
+        id: 35,
+        name: 'Dragon Memories',
+    },
+    {
+        id: 36,
+        name: 'Throne Of Want',
+    },
+];
